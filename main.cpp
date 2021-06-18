c4708d7224f7b8da0ecb2bff52b5cb736c2e3aba
stellarfloat <66378218+stellarfloat@users.noreply.github.com>
2021-06-18 19:11:37 +0900

    Update color palette

--- main.cpp
+++ main.cpp
@@ -53,19 +53,25 @@ void init_display() {
     start_color();
 
     // definition for ncurses color pair id
-    #define COLOR_ID_EMPTY      0
-    #define COLOR_ID_WALL       1
-    #define COLOR_ID_IWALL      2
-    #define COLOR_ID_SNAKE_HEAD 3
-    #define COLOR_ID_SNAKE_BODY 4
+    #define COLOR_ID_EMPTY       0
+    #define COLOR_ID_WALL        1
+    #define COLOR_ID_IWALL       2
+    #define COLOR_ID_SNAKE_HEAD  3
+    #define COLOR_ID_SNAKE_BODY  4
+    #define COLOR_ID_ITEM_GROWTH 5
+    #define COLOR_ID_ITEM_POISON 6
+    #define COLOR_ID_GATE        7
 
 // initialize color pair
 // init_pair(pair_id, foreground_color, background_color)
     init_pair(COLOR_ID_EMPTY, COLOR_BLACK, COLOR_BLACK);
     init_pair(COLOR_ID_WALL, COLOR_BLACK, COLOR_WHITE);
-    init_pair(COLOR_ID_IWALL, COLOR_BLACK, COLOR_YELLOW);
+    init_pair(COLOR_ID_IWALL, COLOR_BLACK, COLOR_WHITE);
     init_pair(COLOR_ID_SNAKE_HEAD, COLOR_WHITE, COLOR_GREEN);
     init_pair(COLOR_ID_SNAKE_BODY, COLOR_BLACK, COLOR_GREEN);
+    init_pair(COLOR_ID_ITEM_GROWTH, COLOR_WHITE, COLOR_YELLOW);
+    init_pair(COLOR_ID_ITEM_POISON, COLOR_WHITE, COLOR_RED);
+    init_pair(COLOR_ID_GATE, COLOR_WHITE, COLOR_MAGENTA);
 }
 
 void print_map() {
@@ -85,7 +91,7 @@ void print_map() {
                 break;
             case 2:
                 attron(COLOR_PAIR(COLOR_ID_IWALL));
-                mvaddstr(i, 2*j, "  "); // immune wall
+                mvaddstr(i, 2*j, "||"); // immune wall
                 attroff(COLOR_PAIR(COLOR_ID_IWALL));
                 break;
             case 3:
