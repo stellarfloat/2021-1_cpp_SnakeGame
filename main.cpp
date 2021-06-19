d1727d941d4199777790f59887098b71410197f0
yoonseok <gtavexir@kookmin.ac.kr>
2021-06-19 23:24:53 +0900

    stage 4

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <deque>
-#include <time.h>
+#include <ctime>
 #include <stdlib.h>
 
 #include <locale.h>
@@ -18,12 +18,59 @@ using namespace std;
 using this_thread::sleep_for;
 #define WIDTH 21
 #define HEIGHT 21
+#define itemListLength 3
+const pair<int, int> axisID[] = {make_pair(0, -1), make_pair(-1, 0), make_pair(0, 1), make_pair(1, 0)};
 
 int map_data[HEIGHT][WIDTH]; // temporary global map data
 
-void load_level() {
-    ifstream lv_data;
+class GateManager {
+public:
+    deque<pair<int, int>> wallList;
+    int length;
+    bool onManager;
+    bool usingGate;
+    bool willClose;
+    bool open;
+    pair<int, int> gateIdx;
+    time_t makeTime;
+    time_t delTime;
 
+    GateManager() { wallList.clear(); onManager = false; usingGate = false; open = false; gateIdx = make_pair(-1, -1); delTime = time(NULL); }
+    void managerOn(int sz, time_t t) {
+        if(sz >= 7 || (double)(time(NULL) - t) >= 1)
+            onManager = true;
+    }
+    void makeGate() {
+        time_t nowTime = time(NULL);
+        double r = (double) nowTime - this->delTime;
+        if(r < (double)(rand() % 10)) return;
+        this->open = true;
+        int a = -1, b = -1;
+        while(1) {
+            a = rand() % wallList.size();
+            b = rand() % wallList.size();
+            if(a != b && a >= 0 && a < wallList.size() && b >= 0 && b < wallList.size()) break;
+        }
+        gateIdx = make_pair(a, b);
+        map_data[wallList[a].first][wallList[a].second] = 7;
+        map_data[wallList[b].first][wallList[b].second] = 7;
+        this->makeTime = time(NULL);
+    }
+    void delGate() {
+        time_t nowTime = time(NULL);
+        double r = (double) nowTime - this->makeTime;
+        if(r < 10.) return;
+        this->open = false;
+        int a = this->gateIdx.first, b = this->gateIdx.second;
+        map_data[wallList[a].first][wallList[a].second] = 1;
+        map_data[wallList[b].first][wallList[b].second] = 1;
+        this->gateIdx = make_pair(-1, -1);
+        this->delTime = time(NULL);
+    }
+};
+
+void load_level(GateManager& wall) {
+    ifstream lv_data;
     lv_data.open("LevelData/test.txt");
 
     if (lv_data.fail()) {
@@ -35,8 +82,11 @@ void load_level() {
     while (!lv_data.eof()) {
         char temp[25];
         lv_data.getline(temp, 25);
-        for (int i = 0; i < WIDTH; i++)
+        for (int i = 0; i < WIDTH; i++) {
             map_data[h][i] = (temp[i] - '0');
+            if(map_data[h][i] == 1)
+                wall.wallList.push_back(make_pair(h, i));
+        }
         h++;
     }
 }
@@ -133,12 +183,13 @@ void print_map() {
 class Snake {
 public:
     bool gameEnd = false;
-    char dir;
+    int dir; // 0: left, 1:up, 2:right, 3.down
     int size, headRow, headCol;
+    pair<int, int> inGateAxis;
     deque<pair<int, int>> body;
 
     Snake(int row = 10, int col = 9) {
-        dir = 'l';
+        dir = 0;
         headRow = row;
         headCol = col;
         size = 5;
@@ -152,42 +203,58 @@ public:
         int key = getch();
         switch (key) {
         case KEY_LEFT:
-            if (dir != 'r') dir = 'l';
+            if (dir != 2) dir = 0;
             else gameEnd = true;
             break;
         case KEY_RIGHT:
-            if (dir != 'l') dir = 'r';
+            if (dir != 0) dir = 2;
             else gameEnd = true;
             break;
         case KEY_UP:
-            if (dir != 'd') dir = 'u';
+            if (dir != 3) dir = 1;
             else gameEnd = true;
             break;
         case KEY_DOWN:
-            if (dir != 'u') dir = 'd';
+            if (dir != 1) dir = 3;
             else gameEnd = true;
             break;
         }
         return;
     }
 
-    void update() {
-        if(dir == 'l') body.insert(body.begin(), make_pair(body[0].first, body[0].second - 1));
-        else if (dir == 'r') body.insert(body.begin(), make_pair(body[0].first, body[0].second + 1));
-        else if (dir == 'u') body.insert(body.begin(), make_pair(body[0].first - 1, body[0].second));
-        else if (dir == 'd') body.insert(body.begin(), make_pair(body[0].first + 1, body[0].second));
-
+    void update(GateManager& wall) {
+        bool getGROWTH = false;
+        bool getPOSION = false;
+        body.push_front(make_pair(body[0].first + axisID[dir].first, body[0].second + axisID[dir].second));
         int row = body.front().first, col = body.front().second;
-        if(map_data[row][col] != 0 && map_data[row][col] != 5 && map_data[row][col] != 6) {
+        // 충돌에 의한 게임 종료
+        if(map_data[row][col] == 1 || map_data[row][col] == 4) {
             gameEnd = true;
             return;
         }
+        // Gate 사용
+        if(map_data[row][col] == 7) {
+            inGateAxis = make_pair(row, col);
+            body.pop_front();
+            wall.usingGate = true;
+            int a = wall.gateIdx.first, b = wall.gateIdx.second;
+            pair<int, int> GateAxis = (make_pair(row, col) == wall.wallList[a]) ? wall.wallList[b] : wall.wallList[a];
+            int outDir = dir;
+            for(int i = 0; i < 4; i++) {
+                int outRow = GateAxis.first + axisID[outDir].first;
+                int outCol = GateAxis.second + axisID[outDir].second;
+                if(outRow >= 0 && outRow <= HEIGHT && outCol >= 0 && outCol <= WIDTH && map_data[outRow][outCol] != 1 && map_data[outRow][outCol] != 2 && map_data[outRow][outCol] != 4) {
+                    this->dir = outDir;
+                    body.push_front(make_pair(outRow, outCol));
+                    break;
+                }
+                if(++outDir == 4) outDir = 0;
+            } 
+        }
 
-        bool getGROWTH = false;
-        bool getPOSION = false;
-        if(map_data[row][col] == 5) getGROWTH = true;
-        if(map_data[row][col] == 6) getPOSION = true;
-        
+        // 아이템 획득 판단
+        if(map_data[body.front().first][body.front().second] == 5) getGROWTH = true;
+        if(map_data[body.front().first][body.front().second] == 6) getPOSION = true;
         map_data[body.front().first][body.front().second] = 3;
         for(int i = 1; i < body.size() - 1; i++)
             map_data[body[i].first][body[i].second] = 4;
@@ -198,11 +265,25 @@ public:
             body.pop_back();
             if(body.size() < 3) gameEnd = true;
         }
+        
+        // Gate 사용 끝났는지 확인
+        if(!wall.willClose && wall.usingGate) {
+            pair<int, int> tail = body.back();
+            for(int i = 0; i < 4; i++) {
+                pair<int, int> tmp = make_pair(tail.first + axisID[i].first, tail.second + axisID[i].second);
+                if(tmp == inGateAxis) {
+                    wall.willClose = true;
+                    break;
+                }
+            }
+        }
+        else if(wall.willClose) {
+            wall.willClose = false;
+            wall.usingGate = false;
+        }
     }
 
 };
-
-#define itemListLength 3
 
 struct ItemInfo {
     int row;
@@ -210,7 +291,8 @@ struct ItemInfo {
     int info; // 0 GROWTH, 1 POISON
     bool ttl;
     bool first;
-    clock_t makeTime;
+    time_t makeTime;
+    
 };
 
 class ItemManager {
@@ -246,9 +328,9 @@ public:
         }
         it.first = true;
         it.ttl = true;
-        it.makeTime = clock();
         for(int i = 0; i < itemListLength; i++) {
             if(!itemList[i].ttl) {
+                it.makeTime = time(NULL);
                 map_data[it.row][it.col] = it.info + 5;
                 itemList[i] = it;
                 break;
@@ -257,8 +339,10 @@ public:
     }
     void delItem() {
         for(int i = 0; i < itemListLength; i++) {
-            double res = (double)(clock() - itemList[i].makeTime) / CLOCKS_PER_SEC;
-            if(map_data[itemList[i].row][itemList[i].col] == 0 || res > 0.015) {
+            if(itemList[i].ttl == false) continue;
+            time_t nowTime = time(NULL);
+            double r = (double)(nowTime - itemList[i].makeTime);
+            if(map_data[itemList[i].row][itemList[i].col] == 0 || r >= 5) {
                 itemList[i].ttl = false;
                 map_data[itemList[i].row][itemList[i].col] = 0;
             }
@@ -268,9 +352,12 @@ public:
 
 int main() {
     srand(time(NULL));
-    
+    time_t gameStart = time(NULL);
+
+    GateManager wall = GateManager();
+
     // load map data
-    load_level();
+    load_level(wall);
 
     init_display();
 
@@ -292,11 +379,15 @@ int main() {
         if (kbhit()) {
             user.setDir();
         }
-        user.update();
+        user.update(wall);
         item.makeItem();
         item.delItem();
+        if(!wall.onManager) wall.managerOn(user.body.size(), gameStart);
+        else {
+            if(wall.open && !wall.usingGate) wall.delGate();
+            if(!wall.open) wall.makeGate();
+        }
     }
-
     getch();
     endwin();
     return 0;
