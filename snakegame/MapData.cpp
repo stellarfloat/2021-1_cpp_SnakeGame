3e9baa1dfb35b4403c0969414b79ac98a3bde1c4
stellarfloat <66378218+stellarfloat@users.noreply.github.com>
2021-06-19 17:25:13 +0900

    Refactoring - map display PoC

---
+++ snakegame/MapData.cpp
@@ -0,0 +1,44 @@
+#include <fstream>
+#include <iostream>
+
+#include "MapData.hpp"
+#include "GameConfig.hpp"
+
+
+MapData::MapData() {
+
+}
+
+MapData::~MapData() {
+
+}
+
+void MapData::load(std::string dir) {
+  std::ifstream lv_data;
+
+  lv_data.open(dir);
+
+  if (lv_data.fail()) {
+    std::cerr << "loading error" << std::endl;
+    return;
+  }
+
+  int h = 0;
+  while (h < HEIGHT) {// !lv_data.eof()
+    char temp[25];
+    lv_data.getline(temp, 25);
+    for (int i = 0; i < WIDTH; i++)
+      map_data[h][i] = (temp[i] - '0');
+    h++;
+  }
+}
+
+int MapData::getData(int i, int j) {
+  return map_data[i][j];
+}
+
+void MapData::setData(int i, int j, int newData) {
+  if (newData >= 0 && newData <= 7) { // 맵 요소는 0~7만 존재 
+    map_data[i][j] = newData;
+  }
+}
---
+++ snakegame/MapData.hpp
@@ -0,0 +1,17 @@
+#include <string>
+
+#include "GameConfig.hpp"
+
+
+class MapData {
+
+  int map_data[HEIGHT][WIDTH];
+
+public:
+  MapData();
+  ~MapData();
+
+  void load(std::string dir);              // 맵 파일 경로
+  int getData(int i, int j);               // 인덱스 기반 맵데이터 접근
+  void setData(int i, int j, int newData); // 인덱스 기반 맵데이터 조작
+};
