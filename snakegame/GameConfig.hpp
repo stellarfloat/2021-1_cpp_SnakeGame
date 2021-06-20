8fda82fb0dbde451d507e6e6346f5e2b3e3fe469
stellarfloat <66378218+stellarfloat@users.noreply.github.com>
2021-06-20 16:40:27 +0900

    Add level metaData/missionData

--- snakegame/GameConfig.hpp
+++ snakegame/GameConfig.hpp
@@ -3,6 +3,7 @@
 #ifndef _GAMECONFIG_H_
 #define _GAMECONFIG_H_
 
+#include <string>
 #include <curses.h>
 #include <ncurses.h>
 
@@ -32,5 +33,23 @@ const std::pair<int, int> axisID[] = {
   std::make_pair(-1, 0), 
   std::make_pair(0, 1), 
   std::make_pair(1, 0)
+};
+
+// definition of mission in each level
+// {bodyLength, itemGrowth, itemPoison, gateUsed}
+const int levelMissionData[5][4] = {
+  {0, 0, 0, 0},  // index 0 is empty
+  {7, 1, 1, 1},  // level 1, map1.txt
+  {9, 3, 3, 2},  // level 2, map2.txt
+  {11, 5, 5, 3}, // level 3, map3.txt
+  {11, 7, 7, 4}, // level 4, map4.txt
+};
+
+const std::pair<std::string, std::pair<int, int>> levelMetaData[] = {
+  std::make_pair("", std::make_pair(0, 0)), // index 0 is empty
+  std::make_pair("snakegame/LevelData/map1.txt", std::make_pair(3, 13)), 
+  std::make_pair("snakegame/LevelData/map2.txt", std::make_pair(16, 22)), 
+  std::make_pair("snakegame/LevelData/map3.txt", std::make_pair(16, 18)), 
+  std::make_pair("snakegame/LevelData/map4.txt", std::make_pair(3, 13))
 };
 #endif
